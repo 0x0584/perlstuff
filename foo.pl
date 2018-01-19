efe482bf7e28dd76f3599b7ce06e05d492553de1
Anas Rchid <rchid.anas@gmail.com>
2018-01-19 16:06:03 +0000

    Initial commit.

---
+++ foo.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+#
+#		 AUTHOR: ANAS RCHID (0x0584)
+#           DESCRIPTION: playing around with perl..
+#
+#    CREATED: 01/19/2018
+# 
+
+print @ARGV;
+print "THIS IS A TEST\n";
+
+print "@ARGV\t";
+print 'sss';
+
+print '@ARGV\t';
