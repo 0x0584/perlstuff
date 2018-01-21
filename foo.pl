5eaf59f90563fc1e3a104393d717b4a43a750b9f
Anas Rchid <rchid.anas@gmail.com>
2018-01-21 00:32:41 +0000

    subs are a lot of fun..

--- foo.pl
+++ foo.pl
@@ -7,19 +7,43 @@
 # 
 
 use strict;			# this is a `pragma`
+use warnings;
 
-sub echo {
-    print "@_";			# @_: array of the params
-				# that are passed to the sub
-				# routine 
-				# (while calling it maybe?)
-    print "this is a fucking test";
-}
+# sub echo {
+#     print "@_";			# @_: array of the params
+# 				# that are passed to the sub
+# 				# routine 
+# 				# (while calling it maybe?)
+#     print "this is a fucking test";
+# }
 
 # print "THIS IS A TEST\n";
 # print @ARGV, "another one"."HAHA\n";
 
-print @ARGV, "\n";
-print "@ARGV", "\n";
+# print @ARGV, "\n";
+# print "@ARGV", "\n";
 
-print '@ARGV\t';
+# print '@ARGV\t';
+
+# ========================================================================
+my $ERR = 'ERR';
+
+open my $fin, '<', 'perl.org' or die ($ERR);
+
+while (my $line = <$fin>) {
+     # print chomp $line;       # this is not logical, it would print
+				# the return value of chomp
+     # chomp $line;		# remove '\n'
+     # print $line;
+}
+
+sub foundin {
+     my $dict = $_[0];
+     my $count = 5;
+
+     print $dict;
+
+     return $count;
+}
+
+print foundin 'test';
